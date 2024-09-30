a62eca8559f43c31b789f30a107331743dcd9dc1
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2024-09-30 21:03:08 +0200

    Add files via upload

--- estrazioneNumeri/src/Main.java
+++ estrazioneNumeri/src/Main.java
@@ -1,116 +1,174 @@
 import static Utility.Tools.*;
-
 import java.util.Scanner;
 import java.util.Random;
+
 
 public class Main {
     public static void main(String[] args) {
         Scanner tastiera = new Scanner(System.in);
-        String[] opzioni = {"Menu", "1 Genera Random", "2 Visualizzazione", "3 Rimuovi Numero", "4 rimuovi numero 2", "5 Fine"};
+        String[] opzioni = {"Menu", "1 Genera Random", "2 Visualizzazione", "3 Ricerca","4 Cancella numero", "5 vettore dispari","6 ordina vettore","7 raddoppia vettore","8 Fine"};
         boolean esci = true;
-        final int MAXNUMERI = 10;
+        final int MAXNUMERI = 50;
         int[] nEstratti = new int[MAXNUMERI];
-
+        int numero;
         do {
             switch (Menu(opzioni, tastiera)) {
-                case 1:
+                case 1 ->{
                     System.out.println("Generazione");
                     generaNumeri(nEstratti);
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     System.out.println("Visualizzazione");
-                    for (int i = 0; i < nEstratti.length; i++) {
-                        if (nEstratti[i] != 0) {
-                            System.out.println(nEstratti[i]);
-                        }
+                    visualizza(nEstratti);
+                }
+                case 3 -> {
+                    System.out.println("Inserire il numero da trovare");
+                    numero = trovaNumero(Integer.parseInt(tastiera.nextLine()), nEstratti);
+                    if(numero == -1) {
+                        System.out.println("Numero non trovato");
+                    } else {
+                        System.out.println(numero);
                     }
-                    break;
-                case 3:
-                    System.out.println("Inserisci il numero da rimuovere:");
-                    int numeroDaEliminare = Integer.parseInt(tastiera.nextLine());
-                    rimuoviNumero(nEstratti, numeroDaEliminare, MAXNUMERI);
-                    break;
-                case 4:
-                    System.out.println("Inserisci il numero da rimuovere:");
-                    numeroDaEliminare = Integer.parseInt(tastiera.nextLine());
-                    nEstratti = rimuoviNumero2(nEstratti, numeroDaEliminare, MAXNUMERI);
-                    break;
-                case 5:
+                }
+                case 4 -> {
+                    System.out.println("Inserire il numero da cancellare");
+                    int posizione = cancellaNumero(Integer.parseInt(tastiera.nextLine()), nEstratti);
+                    if(posizione != -1){
+                        System.out.println("L'elemento cancellato era in posizione " + posizione);
+                    } else {
+                        System.out.println("Elemento non presente");
+                    }
+                }
+
+                case 5 -> {
+                    int [] arrayDispari =arrayDispari(nEstratti);
+                    for(int i=0;i<arrayDispari.length;i++){
+                        System.out.println("I dispari sono"+ arrayDispari[i]);
+                    }
+
+                }
+                case 6-> {
+                    ordinaArray(nEstratti);
+                    System.out.println("L'array ordinato è:");
+                    for(int i=0;i< nEstratti.length;i++){
+                        System.out.println(nEstratti[i]);
+                    }
+                }
+
+                case 7-> {
+                    int [] arrayRaddoppiato = raddoppiaArray(nEstratti,MAXNUMERI);
+                    System.out.println("Il nuovo vettore è:");
+                    for(int i=0;i<arrayRaddoppiato.length;i++){
+                        System.out.println(arrayRaddoppiato[i]);
+                    }
+                }
+                case 8 -> {
                     System.out.println("Fine");
                     esci = false;
-                    break;
+                }
             }
         } while (esci);
     }
 
-    public static void rimuoviNumero(int[] array, int numeroDaEliminare, int MAXNUMERI) {
-        boolean trovato = false;
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == numeroDaEliminare) {
-                trovato = true;
+    public static void generaNumeri(int[] array) {
+        Random numeroRandom = new Random();
+        int numero; // numero da generare e inserire nel vettore
+        int cont = 0; // verifica quanti numeri sono presenti senza ripetizioni
+        boolean presente;
 
-                // Sposta tutti gli elementi successivi di una posizione a sinistra
-                for (int j = i; j < array.length - 1; j++) {
-                    array[j] = array[j + 1];
+        do {
+            numero = numeroRandom.nextInt(0, array.length) + 1;
+            presente = false; // true se numero presente, false se no
+
+
+            for (int i = 0; i < cont; i++) { // Controlla se il numero è già presente
+                if (array[i] == numero) {
+                    presente = true;
                 }
-                MAXNUMERI--;
-                break;
             }
-        }
 
-        if (!trovato) {
-            System.out.println("Numero non trovato.");
+
+            if (!presente) { // se presente = true, lo aggiungo al vettore
+                array[cont] = numero;
+                cont++;
+            }
+        } while (cont < array.length);
+    }
+
+    public static void visualizza(int[] array) {
+        int cont = 0;
+        for (int i : array) {
+            System.out.printf("%4d", i);
+            cont++;
+            if (cont == 10) {
+                cont = 0;
+                System.out.println();
+            }
         }
     }
 
-    public static int[] rimuoviNumero2(int[] array, int numeroDaEliminare, int maxNumeri) {
 
-        if (numeroDaEliminare < 1 || numeroDaEliminare > maxNumeri) {
-            System.out.println("Valore fuori intervallo.");
-            return array;
-        }
-
-        int indice = -1;
+    public static int trovaNumero(int num, int[] array) {
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == numeroDaEliminare) {
-                indice = i;
-                break;
+            if (array[i] == num) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int cancellaNumero(int numeroDaTrovare, int[] array){
+        int numero = trovaNumero(numeroDaTrovare, array);
+        if(numero != -1){
+            for(int i = numero; i < array.length - 1; i++){
+                array[i] = array [i+1];
+            }
+            array[array.length-1] = 0;
+        }
+        return numero;
+    }
+
+
+    public static int[] arrayDispari(int[] array) {
+        int contDispari=0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]%2 != 0) {
+                contDispari++;
             }
         }
 
-        if (indice == -1) {
-            System.out.println("Valore non presente nel vettore.");
-            return array;
-        }
-
-        int[] nuovoArray = new int[array.length - 1];
-        for (int i = 0, j = 0; i < array.length; i++) {
-            if (i != indice) {
-                nuovoArray[j++] = array[i];
+        int[] nuovoArray = new int[contDispari];
+        int j=0;
+        for(int i=0;i<array.length;i++){
+            if(array[i]%2!=0){
+                nuovoArray[j] = array[i];
+                j++;
             }
         }
-
-        System.out.println("Valore " + numeroDaEliminare + " rimosso con successo.");
         return nuovoArray;
     }
 
-    public static void generaNumeri(int[] nEstratti) {
-        Random numeroRandom = new Random();
-        int i = 0;
-        while (i < nEstratti.length) {
-            int numero = numeroRandom.nextInt(1, nEstratti.length + 1);
-            boolean doppio = false;
-            for (int j = 0; j < i; j++) {
-                if (nEstratti[j] == numero) {
-                    doppio = true;
-                    break;
+    public static void ordinaArray(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            // Trova il minimo elemento tra array[i] e array[n-1]
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[i]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
                 }
-            }
-            if (!doppio) {
-                nEstratti[i] = numero;
-                i++;
             }
         }
     }
+
+    public static int [] raddoppiaArray(int []array,int max){
+        max = max *2;
+        int [] nuovoArray = new int [max];
+        for(int i=0;i<array.length;i++){
+            nuovoArray[i] = array[i];
+        }
+        return nuovoArray;
+    }
 }
+
