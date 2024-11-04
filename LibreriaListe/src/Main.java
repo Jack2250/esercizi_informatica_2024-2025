e67777d1d198ecf5d853407fb19924c4d5bb44a6
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2024-11-04 09:18:41 +0100

    Add files via upload

--- LibreriaListe/src/Main.java
+++ LibreriaListe/src/Main.java
@@ -1,5 +1,6 @@
 import frontScreen.FrontEnd;
 import mensola.Libro;
+
 import static utility.Tools.*;
 
 import java.util.ArrayList;
@@ -9,8 +10,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner tastiera = new Scanner(System.in);
         final int MAXLIBRI = 3;
-        ArrayList<Libro> mensola =  new ArrayList<>();
-        String[] opzioni = {"Menu", "Inserimento", "Visualizza","Rimuovi Libro","Uscita"};
+        ArrayList<Libro> mensola = new ArrayList<>();
+        String[] opzioni = {"Menu", "Inserimento", "Visualizza", "Rimuovi Libro", "Modifica numero di pagine", "Visualizza libro per prezzo", "Uscita"};
         boolean esci = true;
 
         do {
@@ -24,40 +25,62 @@ public class Main {
                         String titolo = tastiera.nextLine();
 
                         mensola.add(FrontEnd.LeggiLibro(tastiera, autore, titolo));
-                    }else{
+                    } else
                         System.out.println("La mensola è piena");
-                    }
+
                 }
                 case 2 -> {
-                    if(!mensola.isEmpty()){
+                    if (!mensola.isEmpty())
                         mensola.forEach(libro -> System.out.println("Libro: " + libro.formattaDati()));
-                    }else {
+                    else
                         System.out.println("Non ci sono libri da visualizzare");
-                    }
+
                 }
                 case 3 -> {
-                    if(!mensola.isEmpty()){
+                    if (!mensola.isEmpty()) {
                         System.out.println("Inserisci autore del libro da rimuovere:");
                         String autore = tastiera.nextLine();
                         System.out.println("Inserisci titolo del libro da rimuovere:");
                         String titolo = tastiera.nextLine();
-                        if(rimuoviLibro(mensola, autore, titolo)){
-                            System.out.println("Libro rimosso corretamente");
-                        }else{
+                        if (rimuoviLibro(mensola, autore, titolo))
+                            System.out.println("Libro rimosso correttamente");
+                        else
                             System.out.println("Libro non trovato");
-                        }
-                    }else{
+
+                    } else
                         System.out.println("La mensola è vuota");
-                    }
+
                 }
                 case 4 -> {
+                    if (!mensola.isEmpty()) {
+                        System.out.println("Inserisci autore del libro :");
+                        String autore = tastiera.nextLine();
+                        System.out.println("Inserisci titolo del libro :");
+                        String titolo = tastiera.nextLine();
+                        System.out.println("Modifica il numero di pagine:");
+
+                        if (!(modificaNumeroDiPagine(mensola, autore, titolo, tastiera)))
+                            System.out.println("Libro non trovato");
+
+                    } else
+                        System.out.println("La mensola è vuota");
+
+                }
+
+                case 5 -> {
+                    System.out.println("Inserisci il prezzo del libro :");
+                    double prezzo = Double.parseDouble(tastiera.nextLine());
+
+                }
+                case 6 -> {
                     System.out.println("Fine");
                     esci = false;
                 }
             }
         } while (esci);
     }
-    public static boolean rimuoviLibro(ArrayList<Libro> mensola, String autore, String titolo ){
+
+    public static boolean rimuoviLibro(ArrayList<Libro> mensola, String autore, String titolo) {
         for (int i = 0; i < mensola.size(); i++) {
             if (mensola.get(i).autore.equalsIgnoreCase(autore) && mensola.get(i).titolo.equalsIgnoreCase(titolo)) {
                 mensola.remove(i);
@@ -65,5 +88,24 @@ public class Main {
             }
         }
         return false;
+    }
+
+    public static boolean modificaNumeroDiPagine(ArrayList<Libro> mensola, String autore, String titolo, Scanner sc) {
+        for (int i = 0; i < mensola.size(); i++) {
+            if (mensola.get(i).autore.equalsIgnoreCase(autore) && mensola.get(i).titolo.equalsIgnoreCase(titolo)) {
+                mensola.get(i).pagine = Integer.parseInt(sc.nextLine());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static ArrayList<Libro> visualizzaLibroPerPrezzo(ArrayList<Libro> mensola, double prezzo) {
+        ArrayList<Libro> =new ArrayList<>();
+        for (int i = 0; i < mensola.size(); i++) {
+            if (mensola.get(i).PREZZOPAGINA * mensola.get(i).pagine == prezzo) {
+
+            }
+        }
     }
 }
