67f384fea4f7d16b2a3632444ea88d3b45a79909
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2024-12-09 10:08:22 +0200

    Add files via upload

---
+++ Gara/src/Cronometro.java
@@ -0,0 +1,7 @@
+import java.time.LocalTime;
+import java.util.Timer;
+
+public class Cronometro {
+    private LocalTime startTime;
+    private LocalTime endTime;
+}
--- Gara/src/Pilota.java
+++ Gara/src/Pilota.java
@@ -1,41 +1,51 @@
 public class Pilota {
     private String nome;
     private String cognome;
+    private String nazionalita;
 
-    public Pilota(String nome, String cognome) throws Exception {
-        if (nome.isEmpty() || cognome.isEmpty()) {
-            throw new Exception("Stringa vuota");
+    public Pilota(String nome, String cognome, String nazionalita) throws Exception {
+
+        if (isValue(nome) && isValue(cognome) && isValue(nazionalita)) {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.nazionalita = nazionalita;
+        } else {
+            throw new Exception("Invalid data");
         }
-        this.nome = nome;
-        this.cognome = cognome;
+
+    }
+
+    public void setNazionalita(String nazionalita) {
+        this.nazionalita = nazionalita;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getCognome() {
         return cognome;
     }
 
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
+    public String getNazionalita() {
+        return nazionalita;
+    }
+
+    private boolean isValue(String s) {
+        return s != null && !s.isEmpty();
     }
 
     @Override
     public String toString() {
-        return "Nome:    " + nome + "    Cognome:    " + cognome;
+        return String.format("Nome: %s, Cognome: %s, Nazionalità: %s", nome, cognome, nazionalita);
     }
 
+    @Override
     public boolean equals(Object obj) {
         if (obj instanceof Pilota) {
-            return this.nome.equals(((Pilota) obj).nome) && this.cognome.equals(((Pilota) obj).cognome);
+            return this.nome.equals(((Pilota) obj).nome) && this.cognome.equals(((Pilota) obj).cognome) && this.nazionalita.equals(((Pilota) obj).nazionalita);
+        } else {
+            throw new ClassCastException("L'oggetto non è di tipo pilota");
         }
-        return false;
     }
-
 }
