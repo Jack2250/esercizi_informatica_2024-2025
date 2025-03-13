e4a39510b9b2ef26310d214e80c90cde7d527e9b
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2025-03-13 13:20:24 +0100

    Add files via upload

---
+++ IntroInterfacce/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Cubo c1 = new Cubo(7, 2);
+        System.out.println(c1.peso());
+
+        Sfera s1 = new Sfera(2, 2);
+        System.out.println(s1.peso());
+    }
+}
